414cda03e9447599bd88d7f4784e1f1ca9fe2ad7
nazrulcse <nazrulku07@gmail.com>
2016-01-10 18:42:36 +0600

    Redis

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,6 +29,6 @@ Dusoleil::Application.configure do
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.default_url_options = { :host => 'http://192.168.0.55:3000' }
 
-  ENV["REDISTOGO_URL"] = 'http://127.0.0.1:6379/'
+  ENV["REDIS_URL"] = 'http://127.0.0.1:6379/'
 end
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,5 +59,5 @@ Dusoleil::Application.configure do
  
   config.action_mailer.default_url_options = {:host => 'http://hubtrack.pw'}
 
-  ENV["REDISTOGO_URL"] = 'http://127.0.0.1:6379/'
+  ENV["REDIS_URL"] = 'http://127.0.0.1:6379/'
 end
