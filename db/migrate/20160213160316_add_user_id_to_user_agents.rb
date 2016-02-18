7b7434e745af979707152910171389ed7614a34d
nazrulcse <nazrulku07@gmail.com>
2016-02-18 11:39:13 +0600

    Remove index

--- db/migrate/20160213160316_add_user_id_to_user_agents.rb
+++ db/migrate/20160213160316_add_user_id_to_user_agents.rb
@@ -1,7 +1,6 @@
 class AddUserIdToUserAgents < ActiveRecord::Migration
   def self.up
     add_column :user_agents, :user_id, :integer
-    add_index( :user_agents, :user_agent_key)
     remove_index( :user_agents, :user_agent_key)
     add_index( :user_agents, :user_agent_key)
   end
